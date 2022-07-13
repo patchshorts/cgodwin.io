fa9abe647b3dcc9edbca288bd01c86bcf8097d84
patchshorts <patchshorts@users.noreply.github.com>
2022-07-12 22:41:36 -0500

    Delete .vuepress/.temp directory

--- .vuepress/.temp/internal/siteData.js
+++
@@ -1,1 +0,0 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"en-US\",\"title\":\"CGodwin\",\"description\":\"A demo for vuepress-theme-hope\",\"head\":[],\"locales\":{}}")
--- .vuepress/.temp/md-enhance/reveal.js
+++
@@ -1,4 +0,0 @@
-import { reveal, revealMarkdown, revealHighlight, revealMath, revealSearch, revealNotes, revealZoom } from "/mnt/d/Users/Chris Godwin/code/cgodwin-io-github/node_modules/vuepress-plugin-md-enhance/lib/client/reveal";
-
-export const useReveal = () => [reveal(), revealMarkdown(), revealHighlight(), revealMath(), revealSearch(), revealNotes(), revealZoom()
-];
