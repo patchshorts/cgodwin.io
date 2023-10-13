b5e614ade8bf85018be2aad71f3a5803323b2483
Chris Godwin <patchshorts@gmail.com>
2023-10-13 10:32:24 -0500

    .

--- backend/app.py
+++ backend/app.py
@@ -64,51 +64,52 @@ questions_chat_template = Template(
     )
 
 resume_text = """
-Curriculum Vitae
-Christopher Godwin
-
-Certifications: Google Cloud Professional Cloud Architect (GCCPCA), Red Hat Certified Engineer (RHCE - RHEL 6)
-Current Employer: Charles Schwab, Austin, TX
-Previous Employer: International Game Technologies PLC (THE LOTTERY), Austin, Texas
-Self Employment DBA: Fire, Well & Tree, Austin, Texas
-Eldest Employer: Sundaram, LLC, Austin, Texas
-
-Christopher Shaun Godwin is A high-energy and results-driven senior devops and cloud system engineer & engineering manager with demonstrated experience in database administration, systems analysis/systems engineering, data applications services, and network administration.
+Christopher Shaun Godwin
+# Summary
+A high-energy and results-driven senior DevSecOps Manager/Architect with demonstrated experience in database administration, systems analysis/systems engineering, data applications services, and network administration.
 
 Established capabilities in data migration, installations, integrations, end-user support, and project management. Utilizes outstanding technical knowledge to collaborate on solutions and process implementations. An analytical and industrious top performer who contributes to the efficiency and stability of an organization.
 
 #Experience
 My Certification Image
 
+# Certifications
+Google Certified Cloud Professional Cloud Architect(GCCPCA) - https://www.credential.net/077683a1-2cd5-41bc-be5c-e33499ac7b13
+Red Hat Certified Engineer (RHCE)
+# Charles Schwab, Austin, TX
+# Manager of Software Development and Engineering, January 2020-Present
+Driving End-to-End Architecture: Steered the comprehensive architecture of customer login applications on the GCP platform. Coordinated various stages of development, ensuring cohesive and enterprise-scalable solutions that meet organizational objectives.
 
+Crafting Architectural Principles: Collaboratively developed and refined architectural principles, frameworks, and standards, ensuring robust and maintainable enterprise solutions, fostering strategic technological initiatives and alignment across teams.
 
-Manager of Software Development and Engineering, January 2020-Present at Charles Schwab, Austin, TX:
-Produced highly usable and scalable web software solutions using advanced development technologies
-Implemented application deployment architecture for fortune 500 level enterprise scalability
-Increased Communication and decreased blockers at various organizational levels.
-Solving bushiness problems in a dynamic customer-focused team environment
-A commitment to regularly improving the technical and professional skills of the team.
-Overseeing acquiring new skill through the interview process
-Coached team learning emerging technologies and growing the knowledge of team members
-Reviewed the team's pull requests
-Lead planning, and sprint retros, documentation and deployment status reviews
-Defined integration and deployment standards, uses of tools and cloud patterns
-Drove documentation and then approval process for acceptance criteria.
-Drove testing patterns, code patterns and service relationship
-Drove security first planning schemes which became the Cloud culture
-#Sr. Site Reliability Engineer, January 2020-July 2022
-Contract to hire
-Continuous Integration Github Actions
-Continuous Deployment with Harness.io
-Pipeline Migration
-Rapid Learning
-Process Improvement
+Strategic Technology Leadership: Actively engaged in driving strategic technology initiatives, liaising with product managers, architects, engineers, and broader organizational teams, ensuring aligned and synergized efforts towards organizational goals.
 
+Promoting Communication and Teamwork: Advocated and implemented enhanced communication strategies, fostering improved teamwork, alignment, and collaboration across various organizational levels, both internally and externally.
 
-Sr. Site Reliability Engineer, 2015-2020 at International Game Technologies PLC (THE LOTTERY), Austin, Texas:
+Enhancing Product and Operational Excellence: Contributed valuable feedback and insights on security, development, and strategic directions. Influenced long-range product requirements, methodologies, and operational guidelines, prioritizing continuous improvement and reliability.
+
+Innovation and Knowledge Sharing: Engaged in innovative engineering research, creating case studies and prototypes, particularly on Google Cloud Platform, while promoting a culture of knowledge sharing through code authorship and maintenance.
+
+Mentoring and Career Advancement: Took a proactive role in mentoring engineers, providing guidance in technical skills enhancement and career development, ensuring continuous individual and team growth and improvement.
+
+Operational Responsibility: Participated actively in on-call rotations, tackling complex, real-time challenges to maintain operational integrity and service availability, ensuring sustained organizational performance.
+
+Software Development Leadership: Produced refined web software solutions, emphasizing usability and scalability through advanced development technologies, enhancing enterprise scalability and business problem-solving capabilities.
+
+Fostering Skill Development and Improvement: Led initiatives focusing on continuous improvement, skill acquisition, and knowledge enhancement, ensuring teams stay updated with emerging technologies and industry best practices.
+
+Enhancing Security and Standards: Advocated for a security-first approach in planning, setting robust standards, and defining clear acceptance criteria, ensuring a strong foundation for organizational projects and initiatives.
+
+Facilitating Onboarding: Managed and streamlined the onboarding process for new engineers, ensuring a smooth transition and effective integration into organizational processes and projects.
+
+# Sr. Site Reliability Engineer, January 2020-July 2022
+Established and optimized CI/CD infrastructures, promoting process improvement and operational efficiency.
+Implemented innovative solutions for pipeline migration, fostering a culture of continuous learning and improvement.
+# International Game Technologies PLC (THE LOTTERY), Austin, Texas
+# Sr. Site Reliability Engineer, 2015-2020
 Utilized Vagrant and Docker to mirror entire production stacks within laptop for developers, pushing updates to their environments seamlessly.
 Oversaw and approved half of all team merge requests for DevOps code.
-Conducted automation of job creation for several multi-environment systems totaling in hundreds.
+Conducted automation of Jenkins job creation for several multi-environment systems totaling in hundreds.
 Set up, integrated, built, and released stages for 40 lottery projects.
 Worked with applications that required 25 systems mirrored and highly available.
 Sculpted the CI/CD processes used by the team.
@@ -122,12 +123,13 @@ Wrote custom Puppet resources.
 Processed automation of Ssh logins via Python Pexpect and encryption.
 Performed fully-automated deployment to development and testing servers, totally in developers’ hands.
 Wrote Puppet modules and Ruby YARD documentation that deployed over 16 unique applications.
+Served as primary Puppet/Jenkins support person for other teammates.
 Created installation, release, and rollback plans according to diagrams and application design.
 Executed GitLab and Bitbucket server administration, GitLab and Bitbucket CI/CD.
 Received daily calls working from home, with longest at 21 hours.
 Passed FBI fingerprint background check.
-#Fire, Well & Tree, Austin, Texas
-#Senior Developer, DevOps, System Engineer, 2013-2015
+# Fire, Well & Tree, Austin, Texas
+# Senior Developer, DevOps, System Engineer, 2013-2015
 Utilized Vagrant and Docker to mirror app stacks on laptops.
 Guided end-to-end operations from sales to contracts to organizing team to developing.
 Contracted implemented and designating cluster architecture, highly-available fault-tolerant geolocated builds, and cloud provisioning.
@@ -145,17 +147,36 @@ Built core systems and business infrastructure and internal implementation proce
 Reduced deployment time creating custom deployment playbooks, scripts, and modules.
 Set up bonding on several Linux networks.
 Deployed elastic OpenStack cloud for one customer and Foreman for another running PXE boot process and AOE (ATA over Ethernet frame).
-
-#Professional Development
-Google™ Cloud Professional Cloud Architect (GCCPCA), 2021
-Red Hat™ Certified Engineer (RHCE - RHEL 6), 2010
-PuppetLabs™ training, Business Networking Groups, years of freelancing for professionals.
-
-#TECHNICAL SKILLS
-Kubernetes, Docker, RHCE, Linux, DevOps, Continuous Integration, Continuous Deployment, GitOps, Git, Terraform, Docker, Helm, K8s, Kubernetes, Apache Maven, Python, Bash, Harness.io, Argo CD, Bamboo, Node.js, Automation, GCP, AWS, GCP, Red Hat, Shell Scripting, Mongo, JavaScript, Less, Sass/SCSS, Bootstrap, HTML5/CSS/JS, Drupal, Wordpress, Salt Stack, Puppet, Vue.js, CentOS/Red Hat, SCO UNIX, FreeBSD, OpenBSD, Puppet, Ansible, MV*/MVC
-
-#INTERPERSONAL SKILLS
-Wooer, Connectedness, Achiever, Analytical, Relator, Leadership, Teamwork, Command, Problem-solving, Effective communication skills, Self-direction, Drive, Adaptability, Flexibility, Dependability, Conflict resolution, Flexibility, Leadership, Research, Creativity, Work ethic, Integrity
+#Sundaram, LLC, Austin, Texas
+#Development Guru, 2011-2013
+Built responsive web applications using sensible technologies.
+Worked in an agile environment with identical production and development systems. Developed changes in code for continuous implementation and QA.
+Managed 20 different web applications of different volumes, with some built and others maintained.
+Coordinated design and development effort to create one final seamless product.
+Solved and overcame unique programming challenges.
+Designed, built, and maintained websites using authoring and scripting languages, content creation tools, management tools, and digital media.
+Conferred with management and development teams to prioritize needs, resolve conflicts, develop content criteria, and choose solutions.
+Evaluated code to ensure validity, structuring, meeting of industry standards, and compatible with browsers, devices, and operating systems.
+Maintained understanding of current web technologies and programming practices through continuing education, reading, and participation in professional conferences, workshops, and groups.
+Analyzed user needs to determine technical requirements.
+Developed and validated test routines and schedules to ensure test cases mimicked external interfaces and addressed all browser and device types.
+Crated databases that supported web applications and web sites.
+Collaborated with management and users to develop e-commerce strategies and integrate these strategies with web sites.
+# Professional Development
+Actively participated in PuppetLabs™ training to enhance technical proficiency and stay updated with industry best practices.
+Engaged in business networking groups, fostering professional relationships and staying abreast of industry trends and insights.
+Consistently freelanced for professionals, honing practical skills, and gaining diverse industry experience.
+Committed to continuous learning, staying updated with the latest in technology, tools, and best practices in the IT industry.
+Participated in various workshops and conferences, enhancing knowledge and sharing professional expertise.
+# Interpersonal Skills
+Proven leadership skills, effectively managing and mentoring teams towards achieving organizational objectives.
+Exceptional problem-solving abilities, adept at navigating challenges and driving projects to completion.
+Excellent communicator, facilitating clear and open discussions, ensuring alignment and understanding within teams.
+Strong adaptability, thriving in dynamic environments, and effectively managing change.
+Demonstrated conflict resolution skills, ensuring harmonious team environments and fostering collaboration and innovation.
+Displays strong work ethic and integrity, committed to delivering quality and upholding organizational values.
+# Technical Skills
+Gen AI Implementation, Embeddings, Kubernetes, Docker, RHCE, Linux, DevOps, Continuous Integration, Continuous Deployment, GitOps, Git, Terraform, Docker, Helm, K8s, Kubernetes, Apache Maven, Python, Bash, Harness.io, Argo CD, Bamboo, Node.js, Automation, GCP, AWS, GCP, Red Hat, Shell Scripting, Mongo, JavaScript, Less, Sass/SCSS, Bootstrap, HTML5/CSS/JS, Drupal, Wordpress, Salt Stack, Puppet, Vue.js, CentOS/Red Hat, SCO UNIX, FreeBSD, OpenBSD, Puppet, Ansible, MV*/MVC
 """
 
 app = Flask(__name__)
